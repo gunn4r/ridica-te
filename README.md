95f5dc1a442cd62123812cc06e66489604a34956
Gunnar Gabrielson <gunnar.gabrielson@tcnp3.com>
2017-12-08 11:57:31 -0700

    added circle ci, ridicate voice, firebase-tools dep

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 ## RIDICA-Te -- STAND UP
 
+[![CircleCI](https://circleci.com/gh/gunn4r/ridica-te/tree/master.svg?style=svg)](https://circleci.com/gh/gunn4r/ridica-te/tree/master)
+
 Copyright &copy; 2017-2018 TCN, Inc & Gunnar Gabrielson
--- src/App.js
+++ src/App.js
@@ -3,15 +3,28 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  componentDidMount() {
+    const msg = new window.SpeechSynthesisUtterance();
+    const voices = window.speechSynthesis.getVoices();
+    msg.voice = voices[12]; // Note: some voices don't support altering params
+    msg.voiceURI = 'native';
+    msg.text = 'ridica-te';
+    msg.lang = 'ro-RO';
+
+    setTimeout(() => {
+      window.speechSynthesis.speak(msg);
+    }, 1000)
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to RIDICA-TE</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          YEHAW.
         </p>
       </div>
     );
