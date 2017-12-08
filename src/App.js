788c4d5ad70f1fa461fca3bd201877c2826e17e4
Gunnar Gabrielson <gunnar.gabrielson@tcnp3.com>
2017-12-08 13:17:50 -0700

    added florin audio

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import florinRidicate from './audio/florin-ridicate.wav'
 
 class App extends Component {
   componentDidMount() {
-    const msg = new window.SpeechSynthesisUtterance();
-    const voices = window.speechSynthesis.getVoices();
-    msg.voice = voices[12]; // Note: some voices don't support altering params
-    msg.voiceURI = 'native';
-    msg.text = 'ridica-te';
-    msg.lang = 'ro-RO';
+    // const msg = new window.SpeechSynthesisUtterance();
+    // const voices = window.speechSynthesis.getVoices();
+    // msg.voice = voices[12]; // Note: some voices don't support altering params
+    // msg.voiceURI = 'native';
+    // msg.text = 'ridica-te';
+    // msg.lang = 'ro-RO';
 
     setTimeout(() => {
-      window.speechSynthesis.speak(msg);
+      //window.speechSynthesis.speak(msg);
+      this.audioElm.play()
     }, 1000)
+
   }
 
   render() {
     return (
       <div className="App">
+        <audio controls autoplay style={{ display: 'none' }} ref={(audioElm) => { this.audioElm = audioElm }}>
+          <source src={florinRidicate} type="audio/wav" />
+        </audio>
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to RIDICA-TE</h1>
