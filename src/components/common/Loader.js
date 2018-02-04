26d0cce7f5dfcb5a1f0e5b2a21479397070e952f
Gunnar Gabrielson <gunnargabrielson@gmail.com>
2018-02-04 11:15:25 -0700

    tweaked laoder text, tweaked styles on quote text

--- src/components/common/Loader.js
+++ src/components/common/Loader.js
@@ -4,15 +4,20 @@ import { withStyles } from 'material-ui/styles'
 import Typography from 'material-ui/Typography'
 import { LinearProgress } from 'material-ui/Progress';
 
+const loaderText = 'Did you ever hear the tragedy of Darth Plagueis The Wise? I thought not. It’s not a story the Jedi would tell you. It’s a Sith legend. Darth Plagueis was a Dark Lord of the Sith, so powerful and so wise he could use the Force to influence the midichlorians to create life… He had such a knowledge of the dark side that he could even keep the ones he cared about from dying. The dark side of the Force is a pathway to many abilities some consider to be unnatural. He became so powerful… the only thing he was afraid of was losing his power, which eventually, of course, he did. Unfortunately, he taught his apprentice everything he knew, then his apprentice killed him in his sleep. Ironic. He could save others from death, but not himself.'.split(' ')
 
-const loaderText = [
-  'Are you bready for this?',
-  'Setting stuff up...',
-  '...but not really.',
-  'Yawnnnnn',
-  'Did you leave your stove on?',
-  'Did you ever hear the tragedy of Darth Plagueis The Wise? I thought not. It’s not a story the Jedi would tell you. It’s a Sith legend. Darth Plagueis was a Dark Lord of the Sith, so powerful and so wise he could use the Force to influence the midichlorians to create life… He had such a knowledge of the dark side that he could even keep the ones he cared about from dying. The dark side of the Force is a pathway to many abilities some consider to be unnatural. He became so powerful… the only thing he was afraid of was losing his power, which eventually, of course, he did. Unfortunately, he taught his apprentice everything he knew, then his apprentice killed him in his sleep. Ironic. He could save others from death, but not himself.',
-]
+// const loaderText = [
+//   'Are you bready for this?',
+//   'Setting stuff up...',
+//   '...but not really.',
+//   'DILLY DILLY',
+//   'Yawnnnnn',
+//   'Did you leave your stove on?',
+//   'Jenna is a QT pie.',
+//   'CUURRSEEE YOUUUUUUUUU, TIIIIMMMM COOOOOOOOOOOOKKKK!!!!!!!',
+//   'RIDICA-TTTEEEEEEEE!!!!',
+//   'Did you ever hear the tragedy of Darth Plagueis The Wise? I thought not. It’s not a story the Jedi would tell you. It’s a Sith legend. Darth Plagueis was a Dark Lord of the Sith, so powerful and so wise he could use the Force to influence the midichlorians to create life… He had such a knowledge of the dark side that he could even keep the ones he cared about from dying. The dark side of the Force is a pathway to many abilities some consider to be unnatural. He became so powerful… the only thing he was afraid of was losing his power, which eventually, of course, he did. Unfortunately, he taught his apprentice everything he knew, then his apprentice killed him in his sleep. Ironic. He could save others from death, but not himself.',
+// ]
 
 const styles = (theme) => ({
   loaderWrapper: {
@@ -34,7 +39,7 @@ const styles = (theme) => ({
 class Loader extends React.Component {
   constructor(props) {
     super(props)
-    this.delay = 1500
+    this.delay = 150
     this.timeout = null
     this.state = { index: 0 }
   }
